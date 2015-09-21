4beffac3b66f65a4e2b4fbc90a75849a6ae55b3f
Mateus <mateuspo10@gmail.com>
2015-09-21 14:51:00 -0300

    New Home with logo

--- app/src/main/java/br/com/lucasaquiles/demoappsfd2015/MainActivity.java
+++ app/src/main/java/br/com/lucasaquiles/demoappsfd2015/MainActivity.java
@@ -24,41 +24,14 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         textText = (EditText) findViewById(R.id.editTextNome);
 
         button.setOnClickListener(this);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onClick(View v) {
-
         if(v.getId() == button.getId()){
-
-
             Intent i = new Intent(getApplicationContext(), ListResultActivity.class);
             i.putExtra("nome", textText.getText().toString());
             startActivity(i);
-
-            //Toast.makeText(this,"Opa", Toast.LENGTH_SHORT).show();
-
         }
     }
 }
